0562d9f433de970ba48ba37b39714d9a6c6b561c
Ida Casperson <ida_casperson@hotmail.com>
2025-01-02 18:57:36 +0100

    change back my code in context

--- app/context/CheckoutContext.tsx
+++ app/context/CheckoutContext.tsx
@@ -21,15 +21,17 @@ export interface CartContextType {
 
 const CART_LOCAL_STORAGE_KEY = "cart";
 
-export const CartContext = createContext<CartContextType | null>(null);
+export const CartContext = createContext<CartContextType>({
+  cart: [],
+  confirmedCart: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+  updateQuantity: () => {},
+  clearCart: () => {},
+  setConfirmedCart: () => {},
+});
 
-export const useCart = () => {
-  const context = useContext(CartContext);
-  if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
-  }
-  return context;
-};
+export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }: PropsWithChildren<{}>) => {
   const [cart, setCart] = useState<CartItem[]>([]);
