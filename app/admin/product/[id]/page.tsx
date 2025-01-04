2b0f669cc856aaaeaf94c6440444d22edb8e444c
Ida Casperson <ida_casperson@hotmail.com>
2025-01-04 11:14:26 +0100

    back to my code for page

--- app/admin/product/[id]/page.tsx
+++ app/admin/product/[id]/page.tsx
@@ -1,68 +1,15 @@
-// import db from "@/prisma/db";
-// import { Container } from "@mui/material";
-// import EditProductForm from "../../components/EditProductForm";
-
-// type Props = { params: { id: number } };
-
-// export default async function UpdateExistProduct({ params }: Props) {
-//   const { id } = params;
-
-//   // Fetch produkten från databasen med id som matchar id i URL:en
-//   const product = await db.product.findFirst({ where: { id: Number(id) } });
-
-//   const categories = await db.category.findMany();
-
-//   if (!product) {
-//     return (
-//       <Container
-//         fixed
-//         component="main"
-//         sx={{
-//           display: "flex",
-//           alignItems: "center",
-//           justifyContent: "center",
-//           marginTop: "30px",
-//           marginBottom: "30px",
-//         }}
-//       >
-//         <p>Product not found</p>
-//       </Container>
-//     );
-//   }
-
-//   return (
-//     <Container
-//       fixed
-//       component="main"
-//       sx={{
-//         display: "flex",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         marginTop: "30px",
-//         marginBottom: "30px",
-//       }}
-//     >
-//       <EditProductForm categories={categories} product={product} />
-//     </Container>
-//   );
-// }
-
-// app/admin/product/[id]/page.tsx
-
 import db from "@/prisma/db";
 import { Container } from "@mui/material";
 import EditProductForm from "../../components/EditProductForm";
 
-export default async function UpdateExistProduct({
-  params,
-}: {
-  params: { id: string };
-}) {
-  // Hämta produkt och kategorier på servern
+type Props = { params: { id: number } };
+
+export default async function UpdateExistProduct({ params }: Props) {
   const { id } = params;
 
-  // Hämtar produkt och kategorier
+  // Fetch produkten från databasen med id som matchar id i URL:en
   const product = await db.product.findFirst({ where: { id: Number(id) } });
+
   const categories = await db.category.findMany();
 
   if (!product) {
