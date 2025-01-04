10491c1e140860e020825779b579d369d4f8eb7a
Ida Casperson <ida_casperson@hotmail.com>
2025-01-04 11:12:41 +0100

    test

--- app/admin/product/[id]/page.tsx
+++ app/admin/product/[id]/page.tsx
@@ -47,41 +47,24 @@
 //   );
 // }
 
+// app/admin/product/[id]/page.tsx
+
 import db from "@/prisma/db";
 import { Container } from "@mui/material";
-import { Category, Product } from "@prisma/client";
-import { GetServerSideProps } from "next";
 import EditProductForm from "../../components/EditProductForm";
 
-type Props = {
-  params: { id: string }; // params.id kommer vara en string här
-  product: Product | null; // Vi hanterar produktens data
-  categories: Category[]; // Kategorierna vi hämtar
-};
+export default async function UpdateExistProduct({
+  params,
+}: {
+  params: { id: string };
+}) {
+  // Hämta produkt och kategorier på servern
+  const { id } = params;
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const { id } = params as { id: string }; // Definiera id som string
-
-  // Hämta produkt och kategorier
+  // Hämtar produkt och kategorier
   const product = await db.product.findFirst({ where: { id: Number(id) } });
   const categories = await db.category.findMany();
 
-  if (!product) {
-    return {
-      notFound: true, // För att hantera om produkten inte finns
-    };
-  }
-
-  return {
-    props: {
-      params: { id },
-      product,
-      categories,
-    },
-  };
-};
-
-export default function UpdateExistProduct({ product, categories }: Props) {
   if (!product) {
     return (
       <Container
